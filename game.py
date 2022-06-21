35ec47662bf7f0e4791b60a0f75c03780bc9cf18
Thomas Leiter <thomas.leiter@gmx.de>
2022-06-21 19:56:44 +0200

    Game fields changed to private name convention.

--- game.py
+++ game.py
@@ -11,28 +11,28 @@ from constants_and_states import GameState, Commands, GRID_SIZE
 
 class Game:
     def __init__(self,level_name):
-        self.level_name = level_name
-        self.clock = Clock()
+        self._level_name = level_name
+        self._clock = Clock()
         self._init_entities()
         self._init_graphics()
-        self.game_state = GameState.IN_PROGRESS
+        self._game_state = GameState.IN_PROGRESS
 
     def _init_entities(self):
-        self.grid = {}
-        self.entities = {}
-        self.player = None
-        self.monsters = []
+        self._grid = {}
+        self._entities = {}
+        self._player = None
+        self._monsters = []
         self._load_level()
 
     def _init_graphics(self):
         screen_size = (self.grid_width*GRID_SIZE,self.grid_height*GRID_SIZE)
         self.back_ground = load_sprite('background',screen_size)
         self.screen = pygame.display.set_mode(screen_size)
-        pygame.display.set_caption(f'JumpAndRun level {self.level_name}')
+        pygame.display.set_caption(f'JumpAndRun level {self._level_name}')
         self.font = load_font()
 
     def _load_level(self):
-        path = join('assets','levels',f'level_{self.level_name}.txt')
+        path = join('assets','levels',f'level_{self._level_name}.txt')
         self.grid_width = 0
         self.grid_height = 0
         with open(path,'r') as f:
@@ -41,26 +41,26 @@ class Game:
                 for col in row:
                     if col == 'X':
                         entity = Wall((x,y),self)
-                        self.entities[entity.get_id()] = entity
-                        self.grid[x,y] = {entity.get_id()}
+                        self._entities[entity.get_id()] = entity
+                        self._grid[x,y] = {entity.get_id()}
                     elif col == 'U':
                         entity = SpeedPowerup((x,y),self)
-                        self.entities[entity.get_id()] = entity
-                        self.grid[x,y] = {entity.get_id()}
+                        self._entities[entity.get_id()] = entity
+                        self._grid[x,y] = {entity.get_id()}
                     elif col == 'T':
                         entity = Treasure((x,y),self)
-                        self.entities[entity.get_id()] = entity
-                        self.grid[x,y] = {entity.get_id()}
+                        self._entities[entity.get_id()] = entity
+                        self._grid[x,y] = {entity.get_id()}
                     elif col == 'P':
                         entity = Player((x,y),self)
-                        self.entities[entity.get_id()] = entity
-                        self.grid[x,y] = {entity.get_id()}
-                        self.player = entity
+                        self._entities[entity.get_id()] = entity
+                        self._grid[x,y] = {entity.get_id()}
+                        self._player = entity
                     elif col == 'M':
                         entity = Monster((x,y),self)
-                        self.entities[entity.get_id()] = entity
-                        self.grid[x,y] = {entity.get_id()}
-                        self.monsters.append(entity)
+                        self._entities[entity.get_id()] = entity
+                        self._grid[x,y] = {entity.get_id()}
+                        self._monsters.append(entity)
                     x += 1
                 y += 1
                 self.grid_width = x+1
@@ -68,15 +68,15 @@ class Game:
             self.grid_height = y+1
 
     def _update(self):
-        delta_time = self.clock.tick() / 1000
-        self.player.update(delta_time)
-        for monster in self.monsters:
+        delta_time = self._clock.tick() / 1000
+        self._player.update(delta_time)
+        for monster in self._monsters:
             monster.update(delta_time)
     
     def main_loop(self):
         while True:
             self._handle_inputs()
-            if self.game_state == GameState.IN_PROGRESS:
+            if self._game_state == GameState.IN_PROGRESS:
                 self._update()
             self._draw()
 
@@ -101,52 +101,52 @@ class Game:
 
     def _draw(self):
         self.screen.blit(self.back_ground,(0,0))
-        for id in self.entities:
-            entity = self.entities[id]
+        for id in self._entities:
+            entity = self._entities[id]
             _x = entity.physical_x * GRID_SIZE
             _y = entity.physical_y * GRID_SIZE
             sprite = entity.get_current_sprite()
             self.screen.blit(sprite,(_x,_y))
-        if self.game_state == GameState.IS_WON:
+        if self._game_state == GameState.IS_WON:
             text_output('YOU WIN',self.font,self.screen,(0,255,0))
-        elif self.game_state == GameState.IS_LOST:
+        elif self._game_state == GameState.IS_LOST:
             text_output('YOU LOSE',self.font,self.screen,(255,0,0))
         pygame.display.flip()
 
     def _handle_command(self,command):
         if command == Commands.STAND_STILL:
-            self.player.stand_still()
+            self._player.stand_still()
         elif command == Commands.MOVE_RIGHT:
-            self.player.move_right()
+            self._player.move_right()
         elif command == Commands.MOVE_LEFT:
-            self.player.move_left()
+            self._player.move_left()
         elif command == Commands.JUMP:
-            self.player.jump()
+            self._player.jump()
 
     def get_entity(self,grid_position):
-        if grid_position in self.grid:
-            for id in self.grid[grid_position]:
-                yield self.entities[id] 
+        if grid_position in self._grid:
+            for id in self._grid[grid_position]:
+                yield self._entities[id] 
 
     def get_neighborhood(self,grid_position):
         x,y = grid_position
         for dx,dy in [
             (1,0),(1,1),(0,1),(-1,1),
             (-1,0),(-1,-1),(0,-1),(1,-1)]:
-            if (x+dx,y+dy) in self.grid:
-                for id in self.grid[x+dx,y+dy]:
-                    yield self.entities[id]
+            if (x+dx,y+dy) in self._grid:
+                for id in self._grid[x+dx,y+dy]:
+                    yield self._entities[id]
 
     def update_grid(self,entity,old_position,new_position):
         """
         Move entity from old_position to new_position.
         """
-        if not new_position in self.grid:
-            self.grid[new_position] = set()
-        self.grid[new_position].add(entity.get_id())
-        self.grid[old_position] -= {entity.get_id()}
-        if not self.grid[old_position]:
-            del self.grid[old_position]
+        if not new_position in self._grid:
+            self._grid[new_position] = set()
+        self._grid[new_position].add(entity.get_id())
+        self._grid[old_position] -= {entity.get_id()}
+        if not self._grid[old_position]:
+            del self._grid[old_position]
     
     def set_game_state(self,game_state):
-        self.game_state = game_state
+        self._game_state = game_state
